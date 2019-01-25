059e44621e595a1b25ffd78d479b4c14974db874
WangWei <1811455433@qq.com>
2019-01-25 23:17:30 +0800

    更新了关于作者的视图，点击设置/关于/武科大专用课表以查看

--- app/src/main/java/com/hihuyang/kb/timetable/AboutAppActivity.java
+++ app/src/main/java/com/hihuyang/kb/timetable/AboutAppActivity.java
@@ -4,11 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AboutAppActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
     }
 }
+
 
